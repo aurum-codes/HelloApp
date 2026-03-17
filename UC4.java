1c397512fc8357cf6b89eb464ba4d22bc5e85746
SwarnadeepChanda <swarnadeep.c17@gmail.com>
2026-03-17 08:48:18 +0530

    Implement HelloApp to concatenate command line args

---
+++ UC4.java
@@ -0,0 +1,16 @@
+public class HelloApp
+  {
+    public static void main(String[] args)
+    {
+      StringBuilder nameBuilder = new StringBuilder();
+      for(int i=0; i<args.length; i++)
+        {
+          nameBuilder.append(args[i]);
+          if(i<args.length-1)
+          {
+            nameBuilder.append(", ");
+          }
+          name = nameBuilder.toString();
+        }
+    }
+  }
